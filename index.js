04ddcfe8d893a06204d7d02ce2cee20161cf1a66
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-10 10:07:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let drow = word.length
+  for (let i = 0; i < word.length/2; i++) {
+    if (word[i] !== word[drow-1-i]) {
+      return false
+    }
+    else{
+      return true
+    }
+  }
+
 }
+  // Write your algorithm here
+  // write a function that with an arguement.
+  // compare the arguement to see if it is a palindrome. 
+  // if the passed arguement is a pallindrome, return true.
+
+  // test code
+
+//   const string = ''
+//   if (string[string.length-1] !== string[0]){
+//     return false
+//   }
+//   else([string.length/2 -1]!==string[string.length/2 + 1]){
+//     return false
+//   }
+//   else{
+//     return true
+//   }
+// }
 
 /* 
   Add your pseudocode here
+  initialize an empty string
+  write a condition that compare char within the string using indices
+  return true for equal values and false for none equal values
+
 */
 
 /*
   Add written explanation of your solution here
+  [string.length-1] will be used to identify the last char in the string. 
+  [0] will be the one to identify the first char.
+  comapring the two and bringing true should make the
 */
 
 // You can run `node index.js` to view these console logs
